15e39f99dbbf9e8b19c6c0a1cf0ee39e0d8c1b67
OkanZengin <44057708+OkanZengin@users.noreply.github.com>
2023-08-24 17:52:01 +0300

    operators added

--- part_1/incr_decr_op.js
+++ part_1/incr_decr_op.js
@@ -10,4 +10,23 @@ console.log(`number two, no inc/dec: ${numbertwo}`);
 console.log(`number one, decrement after number : ${numberone--}`);
 console.log(`number two, decrement before number: ${--numbertwo}`);
 console.log(`number one, no inc/dec: ${numberone}`);
-console.log(`number two, no inc/dec: ${numbertwo}`);
+console.log(`number two, no inc/dec: ${numbertwo}`); 
+
+console.log("-----------------");
+
+for(var i =1; i<15; i++ ){
+    if(i<6){
+        console.log("i is less than 6 " + i);
+        if(i == 3){
+            console.log("i is 3 now");
+        }else if(i!=4){
+            console.log("i is not 4");
+            }
+        }
+    else if(i==6){
+        console.log("i is 6 " + i);
+    } else if(i>6){
+        console.log("i is greater than 6 " + i);
+    }
+};
+
