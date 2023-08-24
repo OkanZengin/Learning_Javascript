ffab8d5260e9a7f836ceba9dc17ab2847d3b73f3
OkanZengin <44057708+OkanZengin@users.noreply.github.com>
2023-08-24 17:44:07 +0300

    operators added

---
+++ part_1/incr_decr_op.js
@@ -0,0 +1,13 @@
+var numberone=132;
+var numbertwo=3456;
+
+console.log(`number one, originally : ${numberone}`);
+console.log(`number two, originally : ${numbertwo}`);
+console.log(`number one, increment after number : ${numberone++}`);
+console.log(`number two, increment before number: ${++numbertwo}`);
+console.log(`number one, no inc/dec: ${numberone}`);
+console.log(`number two, no inc/dec: ${numbertwo}`);
+console.log(`number one, decrement after number : ${numberone--}`);
+console.log(`number two, decrement before number: ${--numbertwo}`);
+console.log(`number one, no inc/dec: ${numberone}`);
+console.log(`number two, no inc/dec: ${numbertwo}`);
--- part_1/while_loop.js
+++ part_1/while_loop.js
@@ -5,3 +5,22 @@ while (i<names.length){
     console.log(`index ${i} : ${names[i]}`);
     i = i+1;    
 }
+
+i = 0;
+do{
+    console.log(`index ${i} : ${names[i]}`);
+    i = i+1;
+}
+while(i !=7);
+
+
+i = 0;
+while(true){
+    if (i == 10){
+        break;
+    } 
+    else{
+    console.log(`index ${i} : ${names[i]}`);
+    i = i+1;
+}
+}
